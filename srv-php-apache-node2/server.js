72a97081002cc1aa25e33210cef9ad606d52ec00
marnvel <ricardom.velarde@gmail.com>
2023-08-30 16:31:28 -0300

    Se crean Dockerfiles. 30-08-2023. 16:31hs

---
+++ srv-php-apache-node2/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+const port = 82;
+
+const db = mysql.createConnection({
+	host: "172.17.0.2",
+	user: 'root',
+	password: 'pass',
+	database: 'prueba',
+});
+
+// Conectar a la base de datos MySQL
+db.connect((err) => {
+    if (err) {
+        console.error('Error al conectar a la base de datos:', err);
+        return;
+    }
+    console.log('Conexión exitosa a la base de datos MySQL');
+});
+
+// Ruta raíz que realiza una consulta SQL y responde con una tabla HTML
+app.get('/', (req, res) => {
+    // Realiza una consulta SQL para obtener datos de la tabla 'tu_tabla'
+    const query = 'SELECT * FROM alumnos';
+
+    db.query(query, (err, result) => {
+        if (err) {
+            console.error('Error al ejecutar la consulta SQL:', err);
+            res.status(500).send('Error interno del servidor');
+            return;
+        }
+
+        // Genera la tabla HTML con los datos de la consulta
+        let tableHtml = '<table><tr><th>ID</th><th>Apellidos 2</th><th>Nombres</th><th>DNI</th></tr>';
+        result.forEach((row) => {
+            tableHtml += `<tr><td>${row.id}</td><td>${row.apellidos}</td><td>${row.nombres}</td><td>${row.dni}</td></tr>`;
+        });
+        tableHtml += '</table>';
+
+        // Envía la tabla HTML como respuesta
+        res.send(tableHtml);
+    });
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+    console.log(`Servidor escuchando en el puerto ${port}. Ir a http://localhost:${port}`);
+});
---
+++ srv-php-apache1/index.php
@@ -0,0 +1,69 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Consulta de Alumnos</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+
+<h1>Hola respuesta desde el contenedor srv-php-apache1</h1>
+<h2>Consulta de Alumnos</h2>
+
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Apellidos</th>
+        <th>Nombres</th>
+        <th>DNI</th>
+    </tr>
+
+    <?php
+    $servername = "172.17.0.2";
+    $username = "root";
+    $password = "pass";
+    $dbname = "prueba";
+
+    // Crear conexión
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Verificar conexión
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+    }
+
+    // Consulta SQL
+    $sql = "SELECT id, apellidos, nombres, dni FROM alumnos";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // Mostrar los datos en la tabla HTML
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row["id"] . "</td>";
+            echo "<td>" . $row["apellidos"] . "</td>";
+            echo "<td>" . $row["nombres"] . "</td>";
+            echo "<td>" . $row["dni"] . "</td>";
+            echo "</tr>";
+        }
+    } else {
+        echo "No se encontraron resultados.";
+    }
+
+    // Cerrar conexión
+    $conn->close();
+    ?>
+</table>
+
+</body>
+</html>
